1ef150785cb36f7afaa5685068f8bb3aae6d5fe0
Swan Yee Tun <swanyeetun.2000@gmail.com>
2024-02-09 02:12:55 +0630

    remove pre save password hash

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -9,22 +9,6 @@ const userSchema = new mongoose.Schema({
     user_role: { type: String, required: true, default: "staff" },
 });
 
-// Middleware to hash the password before saving
-userSchema.pre('save', async function(next) {
-    const user = this;
-    if (!user.isModified('password')) {
-        return next();
-    }
-
-    try {
-        const hash = await bcrypt.hash(user.password, 10);
-        user.password = hash;
-        next();
-    } catch (error) {
-        return next(error);
-    }
-});
-
 const User = mongoose.model('User', userSchema);
 
 export default User;

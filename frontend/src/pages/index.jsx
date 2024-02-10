bba6bed727a7b32571a9d80c9cdbdf32d2da382e
Swan Yee Tun <swanyeetun.2000@gmail.com>
2024-02-10 12:47:21 +0630

    [ENHANCEMENT] Create Route and Pages

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Login from "./Login";
+import Register from "./Register";
+import Layout from "./Layout";
+import Error404 from "./404";
+
+export {
+    Login,
+    Register,
+    Layout,
+    Error404,
+}
+

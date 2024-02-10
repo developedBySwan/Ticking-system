1219400be6cfed2eb66d306e3aea038c301f9235
Swan Yee Tun <swanyeetun.2000@gmail.com>
2024-02-10 22:19:09 +0630

    Update Permission Handler condition

--- backend/src/middlewares/permissionHandler.js
+++ backend/src/middlewares/permissionHandler.js
@@ -1,11 +1,10 @@
-
 export default function authorize(permission) {
-    return (req, res, next) => {
-        // Check if user has the required permission
-        if (req.user?.role?.permissions?.includes(permission)) {
-            return next(); // User has permission, proceed to the next middleware
-        } else {
-            return res.status(403).json({ message: 'Unauthorized' }); // User doesn't have permission
-        }
-    };
+  return (req, res, next) => {
+    // Check if user has the required permission
+    if (req.user?.role?.permissions?.includes(permission)) {
+      return next(); // User has permission, proceed to the next middleware
+    } else {
+      return res.status(403).json({ message: "Unauthorized" }); // User doesn't have permission
+    }
+  };
 }

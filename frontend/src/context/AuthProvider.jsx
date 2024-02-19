ba57ea3b140e5259c7ae9fe7ae12f3d8b1025ca6
Swan Yee Tun <swanyeetun.2000@gmail.com>
2024-02-19 21:53:18 +0630

    logout api add

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -4,24 +4,31 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useAxiosFetch from "../hooks/FetchApi";
+import { axiosInstance } from "../services/axios";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const {
+    data: resData,
+    loading,
+    error,
+    fetchData,
+  } = useAxiosFetch(axiosInstance);
   const navigate = useNavigate();
   const getUserAuth = JSON.parse(localStorage.getItem("jwtToken"));
   const [auth, setAuth] = useState(getUserAuth || null);
 
-  const logout = () => {
+  const logout = async () => {
+    await fetchData("user/logout", "post");
+
     localStorage.removeItem("jwtToken");
+
     navigate("/login");
   };
 
-  const value = {
-    auth,
-    setAuth,
-    logout,
-  };
+  const value = { auth, setAuth, logout };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
